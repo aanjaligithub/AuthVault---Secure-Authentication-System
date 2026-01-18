4b49f5aef22cad345e6170e9ea80c932603c3b72
aanjaligithub <aanjalichaurasia15@gmail.com>
2026-01-19 04:11:28 +0530

    update the frontendurl

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://authvault-frontend.onrender.com',
     credentials:true
 }))
 
