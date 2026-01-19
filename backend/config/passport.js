305c8ef2fa5c7e43640c0733f222ec4f493a685a
aanjaligithub <aanjalichaurasia15@gmail.com>
2026-01-19 14:30:31 +0530

    update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,9 +3,9 @@ import  {Strategy as GoogleStrategy} from "passport-google-oauth20"
 import { User } from "../models/userModel.js";
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    clientID:process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL:process.env.GOOGLE_CALLBACK_URL,
   },
   async(accessToken, refreshToken, profile, cb)=> {
     console.log(profile);
